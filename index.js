8ee166f3b3991c9cba12e6295b6fae8a40f00360
Mohammad Sakib <mohammadsakib7679@gmail.com>
2023-04-29 01:28:20 +0600

    three types of data created

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const catagories = require('./data/Catagories.json');
+const news = require('./data/News.json');
 
 app.use(cors())
 
@@ -14,6 +15,29 @@ app.get ('/', (req, res)=>{
 app.get ('/catagories', (req, res)=>{
     res.send(catagories);
 });
+
+app.get ('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    const selectedNews = news.find(n => parseInt(n._id) === id);
+    res.send(selectedNews);
+})
+
+app.get('/catagories/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    console.log(id);
+    if(id===0){
+        res.send(news);
+    }
+    else{
+        const catagoryNews = news.filter(n => parseInt(n.category_id) === id);
+        res.send(catagoryNews);
+    }
+})
 
 app.listen(port, ()=>{
     console.log(`dragon news APi are running on ${port}`)
